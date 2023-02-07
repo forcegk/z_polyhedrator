18de5e7428b08d6743a818f623a5c506306690b3
Alonso Rodriguez <alonso.rodriguez@udc.es>
2023-02-07 23:29:31 +0100

    Experimental changes

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use sprs::{TriMatI, TriMat, CsMat};
 use text_io::scan;
 
 type Priority = u32;
-type Piece = (u32, i32, i32);
+type Piece = (i32, i32, i32);
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -35,10 +35,12 @@ fn main() {
     println!("Opening matrixmarket file: {}", matrixmarket_file_path);
     let base_matrix: CsMat<f64> = sprs::io::read_matrix_market(matrixmarket_file_path).unwrap().to_csr();
     let nonzeros: usize = base_matrix.nnz();
+    let numrows: usize = base_matrix.rows();
+    let numcols: usize = base_matrix.cols();
     
     /* -------- EXPLORE -------- */
     // Create a matrix of the same size as the base matrix with u32 values for the priority computation
-    let mut explored_matrix: TriMat<u32> = TriMat::new((base_matrix.rows(), base_matrix.cols()));
+    let mut explored_matrix: TriMat<u32> = TriMat::new((numrows, numcols));
     explored_matrix.reserve_exact(nonzeros);
     base_matrix.iter().for_each(|(_, (row, col))| {
         explored_matrix.add_triplet(row, col, std::u32::MAX);
@@ -46,42 +48,49 @@ fn main() {
 
     // explored_matrix.set_triplet(triplet_index, row, col, val)
     // explored_matrix.find_locations(row, col)
-    // dbg!(explored_matrix.find_locations(3, 3));  -- [src\main.rs:49] explored_matrix.find_locations(3, 3) = []
+    // dbg!(explored_matrix.find_locations(3, 3));  //-- [src\main.rs:49] explored_matrix.find_locations(3, 3) = []
+    // dbg!(explored_matrix.find_locations(1, 1));  //-- [src\main.rs:49] explored_matrix.find_locations(3, 3) = []
+    // dbg!(explored_matrix.find_locations(1, 1)[0].0);
+
+    // explored_matrix.set_triplet(explored_matrix.find_locations(1, 1)[0], 1, 1, 45);
 
     let mut flag_invalidate = false;
     base_matrix.iter().enumerate().for_each(|(it, (_, (row, col)))| {
-        println!("Iteration {:?} = {:?}. Value is {}", it, (row,col), explored_matrix.data()[it]);
 
-        // // If invalidation has occurred, only check data with MAX value 
-        // if flag_invalidate && explored_matrix.data()[it] != std::u32::MAX {
-        //     return;
-        // }
+        // If invalidation has occurred, only check data with MAX value 
+        if flag_invalidate && explored_matrix.data()[it] != std::u32::MAX {
+            return;
+        }
 
-        // let x;
-        // for (n, i, j) in patterns {
-        //     let mut found = false;
-        //     for ii in (0..n) {
-        //         explored_matrix.find_locations(row, col)
-        //     }
-        // }
-        // // explored_matrix.find_locations(row, col)  // not neccessary as we have the index from the enumeration
-        // explored_matrix.set_triplet(it, row, col, prio);
-        
+        // TODO remove this patterns.clone()     vvvvvvv
+        let mut found: bool;
+        'outer: for (prio, (n, i, j)) in patterns.iter().enumerate() {
+            found = true;
+            for ii in 0..*n {
+                let location = explored_matrix.find_locations(((row as i32) + (ii* (*i))) as usize % numrows, (col as i32 + (ii* (*j))) as usize % numcols);
 
+                if location.is_empty() {
+                    found = false;
+                    break;
+                }
 
-        // let value = explored_matrix.find_locations(row, col);
-        // println!("Value: {:?}", value);   // This returns -- Value: [TripletIndex(97)]
+                if location[0].0 < prio {
+                    break 'outer;
+                }
+            }
+            if found {
+                for ii in 0..*n {
+                    // We have the certainty that each location is nonzero
+                    let location = explored_matrix.find_locations(((row as i32) + (ii* (*i))) as usize % numrows, (col as i32 + (ii* (*j))) as usize % numcols);
 
-
-
-
-
-
-        // patterns.iter().for_each(|(n,i,j)| {
-        //     print!("{} {} {} ", n, i, j);
-        // });
+                    explored_matrix.set_triplet(location[0], ((row as i32) + (ii* (*i))) as usize % numrows, (col as i32 + (ii* (*j))) as usize % numcols, prio as u32);
+                }
+                break 'outer;
+            }
+        }
     });
 
-
-    
+    base_matrix.iter().enumerate().for_each(|(it, (_, (row, col)))| {
+        println!("Iteration {:?} = {:?}. Value is {}", it, (row,col), explored_matrix.data()[it]);
+    });
 }
