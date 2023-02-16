f387d9cc9669f1909de517caa1f969a269b7b0c8
Alonso Rodriguez <alonso.rodriguez@udc.es>
2023-02-16 15:26:18 +0100

    Greedy implementation done

--- src/spgsearx.rs
+++ src/spgsearx.rs
@@ -10,8 +10,8 @@ type Prio = usize;
 const NO_PRIO: Prio = std::usize::MAX;
 
 pub struct SpGSearxMatrix {
-    value_matrix: CsMat<f64>,
-    exploration_matrix: Vec<Prio>,
+    value_matrix: CsMat<bool>,
+    // exploration_matrix: Vec<Prio>,
     nonzeros: usize,
     numrows: usize,
     numcols: usize,
@@ -27,14 +27,31 @@ bitflags! {
 
 impl SpGSearxMatrix {
     pub fn from_file(path: &str) -> SpGSearxMatrix {
-        let value_matrix = sprs::io::read_matrix_market(path).unwrap().to_csr();
+        let f64_value_matrix: CsMat<f64> = sprs::io::read_matrix_market(path).unwrap().to_csc();
+
+        // println!("{:?}", (f64_value_matrix.rows(), f64_value_matrix.cols(), f64_value_matrix.nnz()));
+        // f64_value_matrix.iter().for_each(|(&val, (row, col))| {
+        //     println!("{}: [{},{}]", val, row, col);
+        // });
+
+        let mut value_matrix = CsMat::empty(f64_value_matrix.storage(), f64_value_matrix.inner_dims());
+
+        f64_value_matrix.iter().for_each(|(_, (row, col))| {
+            value_matrix.insert(row, col, false);
+        });
+
         let (numrows, numcols) = (value_matrix.rows(), value_matrix.cols());
         let nonzeros = value_matrix.nnz();
-        let exploration_matrix = vec![NO_PRIO; nonzeros];
+
+        // println!("{:?}", (numrows, numcols, nonzeros));
+        // value_matrix.iter().for_each(|(&val, (row, col))| {
+        //     println!("{}: [{},{}]", val, row, col);
+        // });
+
 
         return SpGSearxMatrix {
             value_matrix: value_matrix,
-            exploration_matrix: exploration_matrix,
+            // exploration_matrix: exploration_matrix,
             nonzeros: nonzeros,
             numrows: numrows,
             numcols: numcols,
@@ -72,29 +89,47 @@ impl SpGSearxMatrix {
         // Parse possible flags
         // let skip_on_invalidation = flags.contains(SpGSearxPatternsFlags::SkipOnInvalidation);
         
+        // Generate access positions
+        let mut nonzero_positions: Vec<(usize, usize)> = Vec::new();
+        self.value_matrix.iter().for_each(|(_, (row, col))| {
+            nonzero_positions.push((row,col));
+        });
+        
         // First pass looking for patterns
-        self.value_matrix.iter().enumerate().for_each(|(_it, (_, (row, col)))| {
-            // let value = self.value_matrix.get(row, col);
-            // self.patterns.iter().for_each(|pattern|{
+        nonzero_positions.iter().enumerate().for_each(|(_it, &(row, col))| {
             let mut found: bool = false;
             let mut piece: Piece = (0,0,(0,0,0));
-            for pattern in self.patterns.iter(){
+            'pattern_search: for pattern in self.patterns.iter(){
                 let result = check_pattern(&self.value_matrix, (row,col), pattern);
                 match result {
                     None => continue,
                     Some(found_piece) => {
                         found = true;
                         piece = found_piece;
-                        break;
+                        break 'pattern_search;
                     },
                 }
             }
 
             if found {
                 // set places to found and add piece
-            }
+                let (x,y,(n,i,j)) = piece;
+                for ii in 0..n {
+                    let pos_val = self.value_matrix.get_mut((x as i64 + (i as i64 * ii as i64)) as usize, (y as i64 + (j as i64 * ii as i64)) as usize).unwrap();
+                    *pos_val = true;
+                }
 
+                self.found_pieces.push(piece);
+            }
         });
+
+        // Add last nonzeros
+        self.value_matrix.iter().for_each(|(&val, (row, col))| {
+            if !val {
+                self.found_pieces.push((row, col, (1, 0, 0)));
+            }
+        });
+
     }
 
     pub fn print_pieces(&self) {
@@ -108,20 +143,31 @@ impl SpGSearxMatrix {
 }
 
 #[inline(always)]
-fn check_pattern(csmat: &CsMat<f64>, curr_pos: (usize, usize), pattern: &Pattern) -> Option<Piece> {
+fn check_pattern(csmat: &CsMat<bool>, curr_pos: (usize, usize), pattern: &Pattern) -> Option<Piece> {
     // println!("Checking pattern {:?}", pattern);
     let &(n,i,j) = pattern;
     let (x,y) = curr_pos;
 
+    // println!("{:?}", (x,y,n,i,j));
+
+    let max_pos_x = (x as i64 + n as i64 * i as i64);
+    let max_pos_y = (y as i64 + n as i64 * j as i64);
+
     // Discard out-of-bounds patterns
-    if (x as i64 + n as i64 * i as i64) >= csmat.rows() as i64 || (y as i64 + n as i64 * j as i64) >= csmat.cols() as i64 {
+    if *csmat.get(x,y).unwrap() || max_pos_x < 0 || max_pos_x >= csmat.rows() as i64 || max_pos_y < 0 || max_pos_y >= csmat.cols() as i64 {
         return None;
     }
 
     for ii in 0..n {
-        let position = csmat.get(x as usize + (i as i64 * ii as i64) as usize, y as usize + (j as i64 * ii as i64) as usize);
+        let position = csmat.get((x as i64 + (i as i64 * ii as i64)) as usize, (y as i64 + (j as i64 * ii as i64)) as usize);
         match position {
-            Some(_) => continue,
+            Some(&is_in_pat) => {
+                if is_in_pat {
+                    return None;
+                } else {
+                    continue;
+                }
+            },
             None    => return None,
         }
     }
