6378ac0e1b58407765793e4b85f9e7163e89eec2
Alonso Rodriguez <alonso.rodriguez@udc.es>
2023-03-24 14:17:40 +0100

    20 lineas :)

--- src/spaugment.rs
+++ src/spaugment.rs
@@ -1,8 +1,20 @@
 use linked_hash_map::LinkedHashMap;
 use crate::utils;
 
+type MetaPattern = (i32,i32);       // Meta-Pattern just describes directionality, and not repeating times.
 type Uwc = (Vec<Vec<i32>>, Vec<i32>, Vec<i32>);
 
 pub struct SpAugment {
     uwc_list: Vec<(usize, Uwc)>,
+    pub nrows: usize,
+    pub ncols: usize,
+    pub nnz: usize,
+    pub inc_nnz: usize,
+    meta_patterns: Vec<(usize,MetaPattern)> // The Meta-Pattern list is only a priority list for meta-pattern search. 
+}
+
+impl SpAugment {
+    pub fn from_uwc_list(uwc_list: Vec<Uwc>, nrows: usize, ncols: usize, nnz: usize) -> Self {
+        
+    }
 }
