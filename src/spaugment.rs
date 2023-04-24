cb4c9790361d625f14742021385a27fc4aa04b66
Alonso Rodriguez <alonso.rodriguez@udc.es>
2023-04-24 20:55:59 +0200

    Initial release for spaugment. TODO integrate with spfgen logic

--- src/spaugment.rs
+++ src/spaugment.rs
@@ -1,13 +1,9 @@
-use std::cmp::max_by_key;
-use std::collections::HashSet;
-use std::time::Instant;
-
 use colored::Colorize;
 use itertools::{Itertools, enumerate};
 use linked_hash_map::LinkedHashMap;
 use sprs::CsMat;
 
-use crate::utils::{Pattern,Piece,Uwc,OriginUwc, pause};
+use crate::utils::{Pattern,Piece,OriginUwc};
 use crate::utils::orig_uwc_to_piece_1d;
 
 //                    N    I    J    Order  Sub-Pattern
@@ -29,10 +25,11 @@ pub struct SpAugment {
 impl SpAugment {
     pub fn from_1d_uwc_list(origin_uwc_list: Vec<(OriginUwc, i32)>, nrows: usize, ncols: usize, nnz: usize) -> Self {
 
-        println!("Printing orig_uwc_list from spaugment!");
-        origin_uwc_list.iter().for_each(|((x,y,(u,w,c)), id)| {
-            print!("({:?},{:?})\t   =\t{:?}\t{:?}\t{:?}\t{:?}\n", x, y, id, u, w, c);
-        });
+        // DEBUG UNCOMMENT
+        // println!("Printing orig_uwc_list from spaugment!");
+        // origin_uwc_list.iter().for_each(|((x,y,(u,w,c)), id)| {
+        //     print!("({:?},{:?})\t   =\t{:?}\t{:?}\t{:?}\t{:?}\n", x, y, id, u, w, c);
+        // });
 
         let meta_pattern_pieces = origin_uwc_list
             .iter()
@@ -51,8 +48,9 @@ impl SpAugment {
             }
         };
 
-        println!("MP_Pieces:\n{:?}", meta_pattern_pieces);
-        println!("MP_Dict:\n{:?}\nLen: {}", meta_patterns, meta_patterns.len());
+        // DEBUG UNCOMMENT
+        // println!("MP_Pieces:\n{:?}", meta_pattern_pieces);
+        // println!("MP_Dict:\n{:?}\nLen: {}", meta_patterns, meta_patterns.len());
 
         SpAugment { 
             origin_uwc_list: origin_uwc_list,
@@ -64,21 +62,28 @@ impl SpAugment {
         }
     }
 
-    pub fn augment_dimensionality(&mut self, target_dim: i32) {
+    pub fn augment_dimensionality(&mut self, target_dim: usize, piece_cutoff: usize) {
 
+        if piece_cutoff < 2 {
+            panic!("\n{} How are you supposed to make length={} pieces?", "[spaugment]".red().bold(), piece_cutoff);
+        }
+
+        // DEBUG UNCOMMENT
         println!("\n------- AUGMENT DIMENSIONALITY -------\n");
 
         let single_compensation: i64 = match self.meta_patterns.get(&-1) {
             Some(_) => -1i64,
             None => 0i64
         };
-        println!("Compensation = {} (this accounts for elements with id=-1)", single_compensation);
+        // DEBUG UNCOMMENT
+        // println!("Compensation = {} (this accounts for elements with id=-1)", single_compensation);
 
         // PRECONDITION: Metapatterns are not necessarily ordered, but same metapatterns are consecutive in the list
 
         // FIXME Maybe generalize this to support any-dimensional input
         for curr_dim in 2..=target_dim {
-            println!("Searching for {}D", curr_dim);
+            // DEBUG UNCOMMENT
+            // println!("Searching for {}D", curr_dim);
 
             // Aux variables for processing
             let mut origins_list: Vec<(i32, i32)> = vec![];
@@ -99,20 +104,19 @@ impl SpAugment {
                 let opt = metapat_pieces_iter.next();
                 
                 if opt.is_none() || matches!(opt, Some((_,id)) if curr_id != *id) {
-                
+                    // DEBUG UNCOMMENT
                     println!("\n------- compute_metapatterns for id = {} -------", curr_id);
 
                     // Compute metapatterns FIXME parametrize max and min strides
-                    match compute_metapatterns(&mut origins_list, start_id, curr_id) {
+                    match compute_metapatterns(&mut origins_list, piece_cutoff, start_id, curr_id) {
                         Some((l_new_metapats, l_new_metapat_pieces)) => {
-                            println!("Found!");
                             start_id += l_new_metapats.len() as i32;
 
                             // Extend new metapats
                             new_metapats.extend(l_new_metapats);
                             new_metapat_pieces.extend(l_new_metapat_pieces);
                         },
-                        None => println!("No new metapatterns"),
+                        None => {},
                     }
 
                     // And prepare for next batch
@@ -131,7 +135,8 @@ impl SpAugment {
                 origins_list.push((*x as i32, *y as i32));
             }
 
-            println!("Startptr: --. Curr_id: {:?}", curr_id);
+            // DEBUG UNCOMMENT
+            // println!("Startptr: --. Curr_id: {:?}", curr_id);
 
             // Add new_metapat_pieces and new_metapats to current ones
             let new_metapats_len = new_metapats.len() as i32;
@@ -146,6 +151,7 @@ impl SpAugment {
                 .map(|((x,y),id)| ((*x,*y), *id))
                 .collect::<Vec<_>>();
 
+            // DEBUG UNCOMMENT
             println!(" ------ INVALIDATE AND UPDATE ORDER ------ ");
             for ((orig_x, orig_y), low_order_id) in pieces {
                 println!(" - {:?}: {}", (orig_x, orig_y), low_order_id);
@@ -156,7 +162,7 @@ impl SpAugment {
                 let (n,i,j) = mp.0;
 
                 // Update dimensionality of current metapattern
-                self.meta_patterns.get_mut(&low_order_id).unwrap().1 = curr_dim;
+                self.meta_patterns.get_mut(&low_order_id).unwrap().1 = curr_dim as i32;
 
                 // Skip the first one as it is already updated (hashmap propierties)
                 for ii in 1..n {
@@ -166,19 +172,19 @@ impl SpAugment {
                 }
             }
 
-            println!("for loop going from [{} to {})", curr_dim_start_id, curr_dim_start_id + new_metapats_len);
-            println!("MP_Pieces:\n{:?}", self.meta_pattern_pieces);
-            println!("MP_Dict:\n{:?}\nLen: {}", self.meta_patterns, self.meta_patterns.len());
-
+            // DEBUG UNCOMMENT
+            // println!("for loop going from [{} to {})", curr_dim_start_id, curr_dim_start_id + new_metapats_len);
+            // println!("MP_Pieces:\n{:?}", self.meta_pattern_pieces);q
+            // println!("MP_Dict:\n{:?}\nLen: {}", self.meta_patterns, self.meta_patterns.len());
         } // for dims
     }
 }
 
 #[inline(always)]
 #[allow(dead_code)]
-fn compute_metapatterns(origins_list: &mut Vec<(i32, i32)>, start_id: i32, low_order_id: i32) -> Option<(LinkedHashMap<i32, MetaPattern>, LinkedHashMap<MetaPatternPiece, i32>)> {
-
-    println!("Metapatterns: {:?}", origins_list);
+fn compute_metapatterns(origins_list: &mut Vec<(i32, i32)>, piece_cutoff: usize, start_id: i32, low_order_id: i32) -> Option<(LinkedHashMap<i32, MetaPattern>, LinkedHashMap<MetaPatternPiece, i32>)> {
+    // DEBUG UNCOMMENT
+    // println!("Metapatterns: {:?}", origins_list);
 
     let origin_list_len = origins_list.len();
 
@@ -195,7 +201,8 @@ fn compute_metapatterns(origins_list: &mut Vec<(i32, i32)>, start_id: i32, low_o
     let (_,max_col) = *origins_list.iter().max_by_key(|(_,col)| *col).unwrap();
     let (max_row,_) = *origins_list.iter().max_by_key(|(row,_)| *row).unwrap();
 
-    println!("Max col = {}, Max row = {}", max_col, max_row);
+    // DEBUG UNCOMMENT
+    // println!("Max col = {}, Max row = {}", max_col, max_row);
 
     // Get all strides between pieces
     let strides = origins_list
@@ -204,7 +211,8 @@ fn compute_metapatterns(origins_list: &mut Vec<(i32, i32)>, start_id: i32, low_o
         .map(|(a,b)| fn_tuple_sub (*b, *a))
         .collect::<Vec<(i32,i32)>>();
 
-    println!("STRIDES: {:?}", strides);
+    // DEBUG UNCOMMENT
+    // println!("STRIDES: {:?}", strides);
 
     let mut occurrences = strides
         .iter()
@@ -215,7 +223,8 @@ fn compute_metapatterns(origins_list: &mut Vec<(i32, i32)>, start_id: i32, low_o
         .sorted_by_key(|((stride_x, stride_y),reps)| std::cmp::Reverse((*reps , ( -((*stride_x) as i64 * (*stride_x) as i64) ) as i64 - ((*stride_y) as i64 *(*stride_y) as i64) as i64 )))
         .collect::<LinkedHashMap<(i32,i32),u32>>();
 
-    println!("OCCURRENCES: {:?}", occurrences);
+    // DEBUG UNCOMMENT
+    // println!("OCCURRENCES: {:?}", occurrences);
 
     // Compose sparse matrix with origins_list
     let mut expl_matrix: CsMat<bool> = CsMat::zero((max_row as usize, max_col as usize));
@@ -223,9 +232,10 @@ fn compute_metapatterns(origins_list: &mut Vec<(i32, i32)>, start_id: i32, low_o
         expl_matrix.insert(*row as usize, *col as usize, false);
     }
 
-    println!("Mat = {:?}", expl_matrix);
+    // DEBUG UNCOMMENT
+    // println!("Mat = {:?}", expl_matrix);
 
-    let mut best_piece: Piece = (0,0,(1,0,0));
+    let mut best_piece: Piece = (0,0,((piece_cutoff-1) as i32,0,0));
     let mut found_piece: bool = false;
     // let origin_list_len (from previously)
 
@@ -248,7 +258,7 @@ fn compute_metapatterns(origins_list: &mut Vec<(i32, i32)>, start_id: i32, low_o
         }
 
         // Reset best_piece and found_piece
-        best_piece = (0,0,(1,0,0));
+        best_piece = (0,0,((piece_cutoff-1) as i32,0,0));
         found_piece = false;
 
         'L2: for (((stride_x, stride_y), n), ((_, _), next_n)) in occurrences.iter().circular_tuple_windows::<((&(i32, i32), &u32), (&(i32, i32), &u32))>() {
@@ -271,7 +281,7 @@ fn compute_metapatterns(origins_list: &mut Vec<(i32, i32)>, start_id: i32, low_o
                 }
             } // 'L3
 
-            if best_piece.2.0 > 1 {
+            if best_piece.2.0 >= (piece_cutoff as i32) {
                 /*** APPEND ROUTINE ***/
                 // Get suitable pattern id
                 let pat_id: i32 = match MetaPatternList.back() {
@@ -305,8 +315,9 @@ fn compute_metapatterns(origins_list: &mut Vec<(i32, i32)>, start_id: i32, low_o
         }
     }
 
-    println!("{}: MetaPatternList = {:?}", "[DEBUG]".cyan().bold(), MetaPatternList);
-    println!("{}: MetaPatternPieceList = {:?}", "[DEBUG]".cyan().bold(), MetaPatternPieceList);
+    // DEBUG UNCOMMENT
+    // println!("{}: MetaPatternList = {:?}", "[DEBUG]".cyan().bold(), MetaPatternList);
+    // println!("{}: MetaPatternPieceList = {:?}", "[DEBUG]".cyan().bold(), MetaPatternPieceList);
 
     if MetaPatternList.len() > 0 {
         return Some((MetaPatternList, MetaPatternPieceList));
